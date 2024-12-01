4c0fb55687968c41b822d5ca367ff290cdf38e1a
Herodiw <rockstarherodiq@gmail.com>
2024-12-01 17:03:33 +0100

    Delete svelte.config.js

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import sveltePreprocess from 'svelte-preprocess';
-
-export default {
-  preprocess: sveltePreprocess({
-    typescript: {
-      transpileOnly: true, // Add this if you want to speed up transpilation
-    }
-  })
-};
