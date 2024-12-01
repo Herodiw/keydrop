e677cb9aeeee50f6543d8a6c138d19f29c722675
Herodiw <rockstarherodiq@gmail.com>
2024-12-01 19:56:18 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';  // lub adapter-node, jeśli chcesz mieć aplikację serwerową
 
 export default {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: '' // Jeżeli Twoja aplikacja jest w subfolderze, ustaw odpowiednią ścieżkę.
-    },
+    adapter: adapter(), // Wybór adaptera (np. adapter-static dla statycznej aplikacji)
+    target: '#svelte',   // Ustalamy cel aplikacji
+    vite: {
+      server: {
+        host: '0.0.0.0', // Słuchanie na wszystkich interfejsach sieciowych
+        port: 5173,      // Port na którym aplikacja będzie działała
+      }
+    }
   },
   preprocess: sveltePreprocess(),
 };
